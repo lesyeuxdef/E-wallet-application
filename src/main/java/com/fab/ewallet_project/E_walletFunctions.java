a130b4dae1ddb0ffb123a1a91fe013c085a62283
lesyeuxdef <decepticonsrcool@gmail.com>
2025-06-03 09:57:59 +0800

    fixed alot of the formatting

---
+++ src/main/java/com/fab/ewallet_project/E_walletFunctions.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.fab.ewallet_project;
+
+/**
+ *
+ * @author cedri
+ */
+interface E_walletFunctions
+{
+    void transfer(Ewallet_frame f);
+    void deposit(Ewallet_frame f);
+    void perish();
+}
